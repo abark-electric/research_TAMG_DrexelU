e6ef97618cff20cf9f7724df17ff3c7c43a7f8a4
Your Name <you@example.com>
2020-11-14 03:10:45 -0500

    Implemented numpy arrays

---
+++ iot_for_dp/constants.py
@@ -0,0 +1,12 @@
+CT_FILE_NAME_DEV = r"ct_2024_tab.txt"
+CT_FILE_KEYS = ['Time (*)', 'FX Time (*)', 'Channel', 'Parametric 1', 'Risetime', 'Counts to Peak', 'Counts', 'Energy',
+                 'Duration', 'Amplitude', 'ASL', 'Threshold', 'Average Frequency', 'RMS(16)', 'Reverberation Frequency',
+                 'Initiation Frequency', 'Signal Strength', 'Absolute Energy', 'Partial Power 1', 'Partial Power 2',
+                 'Partial Power 3', 'Partial Power 4', 'Frequency Centroid', 'Peak Frequency', 'Duration (FX)',
+                 'Amplitude (FX)', 'Counts to Peak (FX)', 'Counts (FX)', 'Energy (FX)', 'Absolute Energy (FX)',
+                 'Signal Strength (FX)', 'Amplitude mV (FX)', 'Risetime (FX)', 'Rise Angle (FX)', 'Decay Angle (FX)',
+                 'Average Frequency (FX)', 'Reverberation Frequency (FX)', 'Initiation Frequency (FX)',
+                 'Zero Crossings (FX)', 'Zero Crossings Frequency (FX)', 'Non Dimentional Amplitude (FX)',
+                 'FFT Amplitude (FX)', 'FFT Peak Frequency (FX)', 'FFT Width @10% (FX)', 'FFT Width @30% (FX)',
+                 'FFT Crossings @30% (FX)', 'Frequency Centroid (FX)', 'Peak Frequency (FX)', 'Peak Power (FX)']
+SKIP_ROW_NUM = 7
